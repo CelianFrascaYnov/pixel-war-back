f607569d583e74e8ec6802b2c06905fe2d7b349e
Célian Frasca <celian.frasca@gmail.com>
2023-10-12 11:06:57 +0200

    ajout de la politique CORS, et de la connexion au front en WS

--- src/main/java/com/pixelwar/webservice/WebserviceApplication.java
+++ src/main/java/com/pixelwar/webservice/WebserviceApplication.java
@@ -1,9 +1,18 @@
 package com.pixelwar.webservice;
 
+import com.pixelwar.webservice.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class WebserviceApplication {
 
 	public static void main(String[] args) {
