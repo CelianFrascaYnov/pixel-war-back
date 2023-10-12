083aba9a1f8c5d697018568a15566b57ab8a3954
Célian Frasca <celian.frasca@gmail.com>
2023-10-12 16:15:50 +0200

    mise en place de la gestion des sessions actives websocket et mise en place d'un vérification BDD

--- src/main/java/com/pixelwar/webservice/handler/WebSocketHandler.java
+++ src/main/java/com/pixelwar/webservice/handler/WebSocketHandler.java
@@ -5,16 +5,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pixelwar.webservice.model.PixelData;
 import com.pixelwar.webservice.service.MongoDBService;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
 
     private final MongoDBService mongoDBService;
+
+    private final List<WebSocketSession> activeSessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        activeSessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        activeSessions.remove(session);
+    }
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // Message reçu du client ici
@@ -26,12 +41,16 @@ public class WebSocketHandler extends TextWebSocketHandler {
         if (jsonNode.has("type")) {
             String messageType = jsonNode.get("type").asText();
 
-            if (messageType.equals("updatePixel")) {
+            if (messageType.equals("ajoutPixel")) {
                 PixelData pixelData = objectMapper.convertValue(jsonNode.get("data"), PixelData.class);
                 mongoDBService.saveData(pixelData);
+                // Envois de la liste mise à jour à toutes les connexions actives
+                for (WebSocketSession activeSession : activeSessions) {
+                    activeSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(mongoDBService.getAllPixels())));
+                }
+            } else if (messageType.equals("getAllPixels")) {
+                session.sendMessage(new TextMessage(objectMapper.writeValueAsString(mongoDBService.getAllPixels())));
             }
         }
-        // Envoi d'une réponse au client
-        // session.sendMessage(new TextMessage("Message reçu"));
     }
 }
--- src/main/java/com/pixelwar/webservice/service/MongoDBService.java
+++ src/main/java/com/pixelwar/webservice/service/MongoDBService.java
@@ -14,10 +14,16 @@ public class MongoDBService {
     private final MongoDBRepository mongoDBRepository;
 
     public void saveData(PixelData pixelData) {
-        try {
+        // Recherche d'un pixel existant avec les mêmes valeurs de x et y
+        PixelData existingPixel = mongoDBRepository.findByXAndY(pixelData.getX(), pixelData.getY());
+
+        if (existingPixel != null) {
+            // Mise à jour de la couleur du pixel existant
+            existingPixel.setColor(pixelData.getColor());
+            mongoDBRepository.save(existingPixel); // Mise à jour du pixel déjà existant
+        } else {
+            // Aucun pixel existant trouvé, insertion d'un nouveau pixel
             mongoDBRepository.save(pixelData);
-        } catch (Exception e) {
-            System.out.println("Erreur : " + e);
         }
     }
 
